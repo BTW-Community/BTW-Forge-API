39f4003d702513adc273786c684455b3a475e8b2
TheWinABagel <gogglebobby@gmail.com>
2026-01-10 23:34:43 -0600

    lets move this to be more managable eh?

--- src/main/java/cpw/mods/fml/common/network/IPacketHandler.java
+++
@@ -1,8 +0,0 @@
-package cpw.mods.fml.common.network;
-
-import net.minecraft.src.INetworkManager;
-import net.minecraft.src.Packet250CustomPayload;
-
-public interface IPacketHandler {
-    void onPacketData(INetworkManager iNetworkManager, Packet250CustomPayload packet250CustomPayload, Player player);
-}
--- src/main/java/cpw/mods/fml/relauncher/SideOnly.java
+++ src/main/java/cpw/mods/fml/relauncher/SideOnly.java
@@ -12,7 +12,7 @@
 
 package cpw.mods.fml.relauncher;
 
-import cpw.mods.fml.common.SidedProxy;
+//import cpw.mods.fml.common.SidedProxy;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -33,6 +33,7 @@ import java.lang.annotation.Target;
  * @author cpw
  *
  */
+@Deprecated(forRemoval = true, since = "Use @Environment instead")
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.TYPE, ElementType.FIELD, ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface SideOnly
